4a240d019899c5c6eeac12a70609694c8d1e7d96
Carlos Vera <carlos.vera@itexico.com>
2019-03-04 11:55:35 -0600

    Add particles and fixe the size bug with home component

--- src/components/Home/homeStyles.js
+++ src/components/Home/homeStyles.js
@@ -1,7 +1,16 @@
 const styles = ({
   position: {
     gridArea: '2 / 1 / 4 / 4 ',
-    backgroundColor: 'red'
+    backgroundColor: 'black'
+  },
+  homeContainer: { 
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'black',
+    '& > div':{
+      height: '100%',
+      width: '100%'
+    }
   }
 })
 
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 // Styles
 import injectSheet from 'react-jss'
 import styles from './homeStyles'
+import Particles from 'react-particles-js'
+
 
 const Home = ({classes}) => {
   return(
     <div className={classes.position}>
-      Home
+      <div className={classes.homeContainer}>
+        <Particles width='100%' height='100%'/>
+      </div>
     </div>
   )
 }
